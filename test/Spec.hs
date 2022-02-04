67f02374f26cb3c075abd29e42ea7db06eab5967
Mario Blažević <blamario@protonmail.com>
2022-02-04 13:35:44 -0500

    Reformatted

--- test/Spec.hs
+++ test/Spec.hs
@@ -11,10 +11,10 @@ import Plutus.V1.Ledger.Scripts (fromCompiledCode)
 import PlutusTx (CompiledCode, applyCode)
 import PlutusTx qualified
 import PlutusTx.Prelude
+import Shrink (shrinkScript)
 import Test.Tasty
 import Test.Tasty.HUnit (testCase, (@?=))
 import Prelude (IO, String)
-import Shrink (shrinkScript)
 import Prelude qualified
 
 printCode :: CompiledCode a -> String
@@ -58,36 +58,36 @@ tests =
   testGroup
     "Test.PlutarchFFI"
     [ testGroup
-      "Simple types"
-      [ testCase "integer literal" $
-          printCode $$(PlutusTx.compile [||42 :: Integer||]) @?= "(program 1.0.0 42)"
-      , testCase "PlutusTx integer function" $
-          printCode double @?= "(program 1.0.0 (\\i0 -> multiplyInteger 2 i1))"
-      , testCase "Plutarch integer function" $
-          printTerm (plam $ \(x :: Term _ PInteger) -> 2 Prelude.* x) @?= "(program 1.0.0 (\\i0 -> multiplyInteger 2 i1))"
-      , testCase "Imported PlutusTx integer function" $
-          printTerm doubleImported @?= "(program 1.0.0 (\\i0 -> multiplyInteger 2 i1))"
-      , testCase "Exported Plutarch integer function" $
-          printCode doubleExported @?= "(program 1.0.0 (\\i0 -> multiplyInteger 2 i1))"
-      , testCase "Imported and applied PlutusTx integer function" $
-          printTerm (plam $ \n-> doubleImported #$ doubleImported # n)
-          @?= "(program 1.0.0 (\\i0 -> (\\i0 -> multiplyInteger 2 i1) (multiplyInteger 2 i1)))"
-      , testCase "Exported and applied Plutarch integer function" $
-          printCode (doubleExported `applyCode` $$(PlutusTx.compile [||21 :: Integer||]))
-          @?= "(program 1.0.0 ((\\i0 -> multiplyInteger 2 i1) 21))"
-      ]
+        "Simple types"
+        [ testCase "integer literal" $
+            printCode $$(PlutusTx.compile [||42 :: Integer||]) @?= "(program 1.0.0 42)"
+        , testCase "PlutusTx integer function" $
+            printCode double @?= "(program 1.0.0 (\\i0 -> multiplyInteger 2 i1))"
+        , testCase "Plutarch integer function" $
+            printTerm (plam $ \(x :: Term _ PInteger) -> 2 Prelude.* x) @?= "(program 1.0.0 (\\i0 -> multiplyInteger 2 i1))"
+        , testCase "Imported PlutusTx integer function" $
+            printTerm doubleImported @?= "(program 1.0.0 (\\i0 -> multiplyInteger 2 i1))"
+        , testCase "Exported Plutarch integer function" $
+            printCode doubleExported @?= "(program 1.0.0 (\\i0 -> multiplyInteger 2 i1))"
+        , testCase "Imported and applied PlutusTx integer function" $
+            printTerm (plam $ \n -> doubleImported #$ doubleImported # n)
+              @?= "(program 1.0.0 (\\i0 -> (\\i0 -> multiplyInteger 2 i1) (multiplyInteger 2 i1)))"
+        , testCase "Exported and applied Plutarch integer function" $
+            printCode (doubleExported `applyCode` $$(PlutusTx.compile [||21 :: Integer||]))
+              @?= "(program 1.0.0 ((\\i0 -> multiplyInteger 2 i1) 21))"
+        ]
     , testGroup
-      "Records"
-      [ testCase "PlutusTx record value" $
-        printShrunkCode $$(PlutusTx.compile [|| SampleRecord False 6 "Hello" ||]) @?= sampleScottEncoding
-      , testCase "Plutarch record value" $
-        printTerm (Rec.rcon $ PSampleRecord (pcon PFalse) 6 "Hello") @?= "(program 1.0.0 (\\i0 -> i1 False 6 \"Hello\"))"
-      , testCase "PlutusTx record function" $
-        printShrunkCode $$(PlutusTx.compile [|| sampleInt ||]) @?= sampleScottField
-      , testCase "Plutarch record function" $
-        printTerm (plam $ \r-> r # Rec.field psampleInt) @?= "(program 1.0.0 (\\i0 -> i1 (\\i0 -> \\i0 -> \\i0 -> i2)))"
-      ]
+        "Records"
+        [ testCase "PlutusTx record value" $
+            printShrunkCode $$(PlutusTx.compile [||SampleRecord False 6 "Hello"||]) @?= sampleScottEncoding
+        , testCase "Plutarch record value" $
+            printTerm (Rec.rcon $ PSampleRecord (pcon PFalse) 6 "Hello") @?= "(program 1.0.0 (\\i0 -> i1 False 6 \"Hello\"))"
+        , testCase "PlutusTx record function" $
+            printShrunkCode $$(PlutusTx.compile [||sampleInt||]) @?= sampleScottField
+        , testCase "Plutarch record function" $
+            printTerm (plam $ \r -> r # Rec.field psampleInt) @?= "(program 1.0.0 (\\i0 -> i1 (\\i0 -> \\i0 -> \\i0 -> i2)))"
+        ]
     ]
-    where
-      sampleScottEncoding = "(program 1.0.0 (delay (\\i0 -> i1 (delay (\\i0 -> \\i0 -> i1)) 6 \"Hello\")))"
-      sampleScottField = "(program 1.0.0 (\\i0 -> force i1 (\\i0 -> \\i0 -> \\i0 -> i2)))"
+  where
+    sampleScottEncoding = "(program 1.0.0 (delay (\\i0 -> i1 (delay (\\i0 -> \\i0 -> i1)) 6 \"Hello\")))"
+    sampleScottField = "(program 1.0.0 (\\i0 -> force i1 (\\i0 -> \\i0 -> \\i0 -> i2)))"
